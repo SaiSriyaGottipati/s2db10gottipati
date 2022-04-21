cba144062e66eed8123ecab620f95b356a2c796e
SaiSriyaGottipati <98078429+SaiSriyaGottipati@users.noreply.github.com>
2022-04-20 23:51:03 -0500

    updated

--- controllers/Vehicle.js
+++ controllers/Vehicle.js
@@ -108,18 +108,46 @@ exports.Vehicle_view_one_Page = async function (req, res) {
         res.status(500)
         res.send(`{'error': '${err}'}`);
     }
-}; 
+};
 
- // Handle building the view for creating a vehicle. 
+// Handle building the view for creating a vehicle. 
 // No body, no in path parameter, no query. 
 // Does not need to be async 
-exports.Vehicle_create_Page =  function(req, res) { 
-    console.log("create view") 
-    try{ 
-        res.render('vehiclecreate', { title: 'Vehicle Create'}); 
-    } 
-    catch(err){ 
-        res.status(500) 
-        res.send(`{'error': '${err}'}`); 
-    } 
+exports.Vehicle_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('vehiclecreate', { title: 'Vehicle Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a vehicle. 
+// query provides the id 
+exports.Vehicle_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Vehicle.findById(req.query.id)
+        res.render('vehicleupdate', { title: 'vehicle Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query 
+exports.Vehicle_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Vehicle.findById(req.query.id)
+        res.render('vehicledelete', {
+            title: 'vehicle Delete', toShow: result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
 }; 
--- routes/Vehicles.js
+++ routes/Vehicles.js
@@ -11,4 +11,11 @@ router.get('/detail', vehicle_controllers.Vehicle_view_one_Page);
 /* GET create costume page */
 router.get('/create', vehicle_controllers.Vehicle_create_Page);
 
+/* GET create update page */ 
+router.get('/update', vehicle_controllers.Vehicle_update_Page); 
+
+/* GET delete costume page */ 
+router.get('/delete', vehicle_controllers.Vehicle_delete_Page); 
+ 
+
 module.exports = router;
